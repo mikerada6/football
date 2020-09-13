6618f13a3ae9f237ab72da450ee1539f02621893
Michael Radaszkiewicz <michael_radaszkiewicz@comcast.com>
2020-09-13 17:16:29 -0400

    intial commit

---
+++ src/test/java/rez/football/football/FootballApplicationTests.java
@@ -0,0 +1,13 @@
+package rez.football.football;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
